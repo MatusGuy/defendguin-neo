5b74d53b0b68323f0e6aa7a6140dec32c8b472ff
MatusGuy <MatusGuy@users.noreply.github.com>
2025-08-04 17:07:52 +0100

    collision automap in tiled

---
+++ assets/src/tilesets/collision.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="collision" tilewidth="16" tileheight="16" tilecount="105" columns="5">
+ <image source="../../../../../Software/supertux-advance/res/gfx/solid.png" width="80" height="336"/>
+</tileset>
